56ba964bab66680d4f90705bcdae100d504fac76
TheRedTanuki <petitmars@outlook.com>
2025-08-13 00:13:39 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Numerics;
+using Raylib_cs;
+class Program
+{
+    static void Main()
+    {
+        Raylib.SetWindowState(ConfigFlags.ResizableWindow);
+        Raylib.InitWindow(0, 0, "Julia");
+        
+        int width = Raylib.GetScreenWidth();
+        int height = Raylib.GetScreenHeight();
+        float aspectRatio = width / height;
+
+        Shader shader = Raylib.LoadShader(null, "ressources/julia_shader.glsl");
+        RenderTexture2D target = Raylib.LoadRenderTexture(width, height);
+
+        Vector3 cameraPosition = new (5.0f, 0.0f, 0.0f);
+        Vector2 cameraAngle = new (0f, 0f);
+
+        int cameraPositionLocation = Raylib.GetShaderLocation(shader, "cameraPosition");
+        int cameraForwardLocation = Raylib.GetShaderLocation(shader, "cameraForward");
+        int cameraRightLocation = Raylib.GetShaderLocation(shader, "cameraRight");
+        int cameraUpLocation = Raylib.GetShaderLocation(shader, "cameraUp");
+        int aspectRatioLocation = Raylib.GetShaderLocation(shader, "aspectRatio");
+
+        Raylib.SetTargetFPS(60);
+        Raylib.SetShaderValue(shader, aspectRatioLocation, aspectRatio, ShaderUniformDataType.Float);
+        Raylib.HideCursor();
+
+        while (!Raylib.WindowShouldClose())
+        {
+            if (Raylib.IsWindowResized())
+            {
+                Raylib.UnloadRenderTexture(target);
+                target = Raylib.LoadRenderTexture(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+                Raylib.SetShaderValue(shader, aspectRatioLocation, aspectRatio, ShaderUniformDataType.Float);
+            }
+
+            var deltaTime = Raylib.GetFrameTime();
+
+            //-- Camera direction -- //
+
+            cameraAngle[0] += Raylib.GetMouseDelta()[0] * deltaTime / 2;
+            cameraAngle[1] -= Raylib.GetMouseDelta()[1] * deltaTime / 2;
+            Raylib.SetMousePosition((int)MathF.Round(width / 2f), (int)MathF.Round(height / 2f));
+
+            // Angles
+            float yaw = cameraAngle[0];
+            float pitch = cameraAngle[1];
+
+            // Forward
+            Vector3 cameraForward = Vector3.Normalize(new(
+                MathF.Cos(pitch) * MathF.Cos(yaw),
+                MathF.Sin(pitch),
+                MathF.Cos(pitch) * MathF.Sin(yaw)));
+
+            // Right
+            Vector3 cameraRight = new(MathF.Sin(yaw), 0, -MathF.Cos(yaw));
+
+            // Up
+            Vector3 cameraUp = Vector3.Cross(cameraForward, cameraRight);
+
+            if (Raylib.IsKeyDown(KeyboardKey.S)) cameraPosition += 5f * deltaTime * cameraForward;
+            if (Raylib.IsKeyDown(KeyboardKey.W)) cameraPosition -= 5f * deltaTime * cameraForward;
+            if (Raylib.IsKeyDown(KeyboardKey.D)) cameraPosition += 5f * deltaTime * cameraRight;
+            if (Raylib.IsKeyDown(KeyboardKey.A)) cameraPosition -= 5f * deltaTime * cameraRight;
+
+            Raylib.SetShaderValue(shader, cameraPositionLocation, cameraPosition, ShaderUniformDataType.Vec3);
+
+            Raylib.SetShaderValue(shader, cameraForwardLocation, cameraForward, ShaderUniformDataType.Vec3);
+            Raylib.SetShaderValue(shader, cameraRightLocation, cameraRight, ShaderUniformDataType.Vec3);
+            Raylib.SetShaderValue(shader, cameraUpLocation, cameraUp, ShaderUniformDataType.Vec3);
+
+            //-- Drawing textures --//
+
+            Raylib.BeginTextureMode(target);
+            Raylib.ClearBackground(Color.Black);
+            Raylib.EndTextureMode();
+
+            Raylib.BeginDrawing();
+            Raylib.BeginShaderMode(shader);
+            Raylib.DrawTexture(target.Texture, 0, 0, Color.White);
+            Raylib.EndShaderMode();
+            Raylib.EndDrawing();
+
+            // Optional : print fps
+            //Console.WriteLine(Raylib.GetFPS());
+        }
+
+        Raylib.UnloadShader(shader);
+        Raylib.CloseWindow();
+    }
+}
---
+++ ressources/julia_shader.glsl
@@ -0,0 +1,99 @@
+#version 330
+
+// This work is based on Keenan Crane paper about 4D julia set
+// https://www.cs.cmu.edu/~kmcrane/Projects/QuaternionJulia/paper.pdf
+
+in vec2 fragTexCoord;
+out vec4 fragColor;
+
+// Basic variables
+uniform float escapeThreshold = 100.0;
+uniform float raymarchingPrecision = 0.001;
+uniform float boundingSphereRadius = 100.0;
+uniform int maxIteration = 50;
+uniform int maxSteps = 128;
+
+// Julia's const
+uniform vec4 c = vec4(-0.2, 0.7, 0.5, 0.0);
+
+// Camera Position
+uniform vec3 cameraPosition = vec3(5.0, 0.0, 0.0);
+
+// Camera Direction
+uniform vec3 cameraForward = vec3(-1.0, 0.0, 0.0);
+uniform vec3 cameraRight = vec3(0.0, 1.0, 0.0);
+uniform vec3 cameraUp = vec3(0.0, 0.0, 1.0);
+
+// Camera and window settings
+uniform float fov = 1.0;
+uniform float aspectRatio = 1.0;
+
+
+//-- Quaternion operations --//
+
+vec4 quatMult(vec4 q1, vec4 q2) {
+    vec4 r;
+    r.x = q1.x * q2.x - dot(q1.yzw, q2.yzw);
+
+    r.yzw = q1.x * q2.yzw + q2.x * q1.yzw + cross(q1.yzw, q2.yzw);
+    return r;
+}
+
+vec4 quatSquare(vec4 q) {
+    vec4 r;
+    r.x = q.x * q.x - dot(q.yzw, q.yzw);
+    r.yzw = 2.0 * q.x * q.yzw;
+    return r;
+}
+
+void iterateQuat(inout vec4 z, inout vec4 dz) {
+    for (int i = 0; i < maxIteration; i++) {
+        dz = 2.0 * quatMult(z, dz);
+        z = quatSquare(z) + c;
+        if (dot(z, z) > escapeThreshold) break;
+    }
+}
+
+//-- Distance estimation --//
+
+float distanceToJulia(vec3 p) {
+    vec4 z = vec4(p, 0.0);
+    vec4 dz = vec4(1.0, 0.0, 0.0, 0.0);
+    iterateQuat(z, dz);
+    float zNorm = length(z);
+    return 0.5 * zNorm * log(zNorm) / length(dz);
+}
+
+//-- Raymarch --//
+
+vec4 raymarch(vec3 rayOrigin, vec3 rayDirection) {
+    while (true) {
+        float dist = distanceToJulia(rayOrigin);
+        if (dist < raymarchingPrecision) {
+            return vec4(normalize(rayOrigin.xyz), 1.0); // return a color based on the 3D coordinates
+        }
+        
+        if (dot(rayOrigin, rayOrigin) > boundingSphereRadius) {
+            return vec4(0.0, 0.0, 0.0, 1.0);
+        }
+        
+        rayOrigin += rayDirection * dist;
+    }
+}
+
+//-- Main --//
+
+void main() {
+    // Convert [0, 1] coordinates to  [-1, 1]
+    vec2 uv = fragTexCoord * 2.0 - 1.0;
+    uv.x *= aspectRatio;
+
+    // Scale for fov
+    float scale = tan(fov * 0.5);
+
+    // Compute final direction of the ray
+    vec3 rayDirection = normalize(-cameraForward + uv.x * scale * cameraRight + uv.y * scale * cameraUp);
+
+    // Throw the ray
+    fragColor = raymarch(cameraPosition, rayDirection);
+}
